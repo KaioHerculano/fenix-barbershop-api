ffa219a33b89fbef5aa44aefa8683057adbdfad1
Kaio Herculano <kaioherculano12@gmail.com>
2026-07-22 20:08:59 -0400

    feat: add appointment model

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 
-from scheduling.models import WorkingHour
+from scheduling.models import Appointment, WorkingHour
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "company",
+        "customer",
+        "barber",
+        "service",
+        "appointment_date",
+        "start_time",
+        "end_time",
+        "status",
+    )
+    search_fields = (
+        "customer__email",
+        "customer__full_name",
+        "barber__user__full_name",
+        "service__name",
+        "company__name",
+        "company__slug",
+    )
+    list_filter = ("company", "barber", "service", "appointment_date", "status")
+    readonly_fields = ("created_at", "updated_at", "cancelled_at", "completed_at")
 
 
 @admin.register(WorkingHour)
